1bd5630f0ccccbe27b592455e47b8039f28a9e8f
wxc <531742655@qq.com>
2018-08-29 11:08:13 +0800

    多线程练习

---
+++ src/main/java/cc/threaddemo/ThreadDemoApplication.java
@@ -0,0 +1,12 @@
+package cc.threaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/cc/threaddemo/ThreadDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package cc.threaddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
